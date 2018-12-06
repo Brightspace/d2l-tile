f5577c8c30295d2ffee75dc416ac84d69f2ad067
Dave Batiste <dave.batiste@d2l.com>
2018-12-06 09:58:18 -0500

    Polymer 3 Conversion

--- d2l-card-footer-link.js
+++ d2l-card-footer-link.js
@@ -9,16 +9,16 @@ Polymer-based web component for links in card footers
   from HTML and may be out of place here. Review them and
   then delete this comment!
 */
-import '../@polymer/polymer/polymer-legacy.js';
+import '@polymer/polymer/polymer-legacy.js';
 
-import '../d2l-colors/d2l-colors.js';
-import '../fastdom/fastdom.js';
-import '../d2l-icons/d2l-icon.js';
-import '../d2l-link/d2l-link-behavior.js';
-import '../d2l-offscreen/d2l-offscreen-shared-styles.js';
-import '../d2l-polymer-behaviors/d2l-focusable-behavior.js';
-import { Polymer } from '../@polymer/polymer/lib/legacy/polymer-fn.js';
-import { dom } from '../@polymer/polymer/lib/legacy/polymer.dom.js';
+import 'd2l-colors/d2l-colors.js';
+import 'fastdom/fastdom.js';
+import 'd2l-icons/d2l-icon.js';
+import 'd2l-link/d2l-link-behavior.js';
+import 'd2l-offscreen/d2l-offscreen-shared-styles.js';
+import 'd2l-polymer-behaviors/d2l-focusable-behavior.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import { dom } from '@polymer/polymer/lib/legacy/polymer.dom.js';
 const $_documentContainer = document.createElement('template');
 
 $_documentContainer.innerHTML = `<dom-module id="d2l-card-footer-link">
--- d2l-card.js
+++ d2l-card.js
@@ -9,18 +9,18 @@ Polymer-based web components for card
   from HTML and may be out of place here. Review them and
   then delete this comment!
 */
-import '../@polymer/polymer/polymer-legacy.js';
+import '@polymer/polymer/polymer-legacy.js';
 
-import '../d2l-colors/d2l-colors.js';
-import '../fastdom/fastdom.js';
-import '../d2l-link/d2l-link-behavior.js';
-import '../d2l-offscreen/d2l-offscreen-shared-styles.js';
-import '../d2l-polymer-behaviors/d2l-dom.js';
-import '../d2l-polymer-behaviors/d2l-focusable-behavior.js';
-import ResizeObserver from '../resize-observer-polyfill/dist/ResizeObserver.es.js';
-import { Polymer } from '../@polymer/polymer/lib/legacy/polymer-fn.js';
-import { dom } from '../@polymer/polymer/lib/legacy/polymer.dom.js';
-import { afterNextRender } from '../@polymer/polymer/lib/utils/render-status.js';
+import 'd2l-colors/d2l-colors.js';
+import 'fastdom/fastdom.js';
+import 'd2l-link/d2l-link-behavior.js';
+import 'd2l-offscreen/d2l-offscreen-shared-styles.js';
+import 'd2l-polymer-behaviors/d2l-dom.js';
+import 'd2l-polymer-behaviors/d2l-focusable-behavior.js';
+import ResizeObserver from 'resize-observer-polyfill/dist/ResizeObserver.es.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import { dom } from '@polymer/polymer/lib/legacy/polymer.dom.js';
+import { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
 const $_documentContainer = document.createElement('template');
 
 $_documentContainer.innerHTML = `<dom-module id="d2l-card">
